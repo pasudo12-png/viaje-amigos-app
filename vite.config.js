eb42098671d797189927a69ca45d0fd4502d4213
unknown <pasudo12@gmail.com>
2026-02-23 20:01:09 -0500

    fix: explicit postcss config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
 })
+```
+
+Presiona **Ctrl+S**, luego en el terminal:
+```
+git add -A
+git commit -m "fix: explicit postcss config"
+git push
